b795a3f732a82a8b37c6c99ab5c8e7bed299281f
kit <kit@DESKTOP-EHKDJE1>
2025-11-08 23:38:07 +0300

    hw3

--- Resoors/resource.h
+++ Resoors/resource.h
@@ -2,27 +2,9 @@
 // Microsoft Visual C++ generated include file.
 // Used by Resource.rc
 //
-#define IDB_BUTTON_0						1000
-#define IDB_BUTTON_1						1001
-#define IDB_BUTTON_2						1002
-#define IDB_BUTTON_3						1003
-#define IDB_BUTTON_4						1004
-#define IDB_BUTTON_5						1005
-#define IDB_BUTTON_6						1006
-#define IDB_BUTTON_7						1007
-#define IDB_BUTTON_8						1008
-#define IDB_BUTTON_9						1009
-#define IDB_BUTTON_POINT					1010
-#define IDB_BUTTON_PLUS						1011
-#define IDB_BUTTON_MINUS					1012
-#define IDB_BUTTON_ASTER					1013
-#define IDB_BUTTON_SLASH					1014
-#define IDB_BUTTON_BSP						1015
-#define IDB_BUTTON_CLR						1016
-#define IDB_BUTTON_EQUAL					1017
-#define ID_MENU_FONT_PULS					4004
-#define ID_MENU_FONT_DOOM					4005
-#define ID_MENU_FONT_DIGIT					4006
+
+
+#define IDR_MENU_FONT_DIGIT					4006
 
 
 
